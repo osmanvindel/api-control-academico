baf4e907e9457f0de244705763d6933ecba8c7bb
osmanvindel <elivid23@gmail.com>
2024-12-08 20:01:53 -0600

    Configuracion CORS

---
+++ src/main/java/com/controlacademico/api_controlacademico/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.controlacademico.api_controlacademico.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("*") // Permite solicitudes desde cualquier origen. Cambia "*" por dominios específicos si lo necesitas
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permite enviar cookies o credenciales
+            }
+        };
+    }
+}
